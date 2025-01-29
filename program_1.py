c15db1161e6e3a026d4004317914e84d4ddf9b59
Tjrosenthal <tjrosenthal@students.unwsp.edu>
2025-01-29 09:30:04 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,8 +1,7 @@
-def personal_information():
 #Tanner Rosenthal
 #1/24/2025
 
-
+def personal_information():
 print("Tanner Rosenthal")
 print("10234 5th ln NW St. Paul, Minnesota, 55101")
 print("Phone Number: 987-654-321")
